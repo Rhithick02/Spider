f0e719b7ba42078529d599399a00d0ae8a74353c
Rhithick <alchemist@pop-os.localdomain>
2021-04-13 00:38:44 +0530

    Heap sort update

--- Algos/Sorting/Heapsort.cpp
+++ Algos/Sorting/Heapsort.cpp
@@ -1,7 +1,6 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
 #include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 #define lli long long
 #define For(i,a,n) for(int i=(a);i<n;i++)
@@ -10,35 +9,60 @@ using namespace std;
 #define pb push_back
 #define fi first
 #define se second
-vector <int> ar;
-void heapify(int parent, int n) {
-    int largest = parent;
-    int leftchild = 2 * parent;
-    int rightchild = 2 * parent + 1;
-    if(leftchild < n && ar[largest] < ar[leftchild])
-        largest = leftchild;
-    if(rightchild < n && ar[largest] < ar[rightchild])
-        largest = rightchild;
-    if(largest != parent) {
-        swap(ar[largest], ar[parent]);
-        heapify(largest, n);
+#define mod 1000000007
+#define MAX 100001
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+void max_heapify(vector <int> &ar, int i, int siz) {
+    int large = i;
+    int left = i * 2 + 1;
+    int right = left + 1;
+    if(left < siz && ar[left] > ar[large]) {
+        large = left;
+    }
+    if(right < siz && ar[right] > ar[large]) {
+        large = right;
+    }
+    if(large != i) {
+        swap(ar[large], ar[i]);
+        max_heapify(ar, large, siz);
     }
 }
-int main(){
-    ios::sync_with_stdio(false);
-    int n, x;
-    cin >> n;
-    For(i, 0, n) {
-        cin >> x;
-        ar.pb(x);
+void increase_key(vector <int> &ar, int i, int key) {
+    ar[i] = key;
+    while(i >= 0 && ar[i/2-1] < ar[i]) {
+        swap(ar[i/2-1] , ar[i]);
+        i = i/2-1;
     }
-    // Building heap
-    for(int i = n/2-1; i >= 0; i--)
-        heapify(i, n);
-    // Removing top element and placing at the end of array
-    for(int i = n-1; i > 0; i--) {
+}
+void heap_increase(vector <int> &ar, int key) {
+    ar.pb(-mod);
+    increase_key(ar, ar.size()-1, key);
+}
+int heap_erase_max(vector <int> &ar) {
+    int max = ar[0];
+    swap(ar[0], ar.back());
+    ar.pop_back();
+    max_heapify(ar, 0, ar.size()-1);
+    return max;
+}
+void heap_sort(vector <int> ar) {
+    /* Sorting by taking the first nummber and swapping with last number and
+       reducing the size of heap by one */
+    for(int i = ar.size() - 1; i > 0; i--) {
         swap(ar[i], ar[0]);
-        heapify(0, i);
+        max_heapify(ar, 0, i);
     }
     for(auto it: ar) cout << it << " ";
+}
+int main() {
+    int n;
+    cin >> n;
+    vector <int> ar(n);
+    for(auto & it: ar) cin >> it;
+    for(int i = n/2-1; i >= 0; i--) {
+        max_heapify(ar, i, n);
+    }
+    heap_increase(ar, 6);
 }
