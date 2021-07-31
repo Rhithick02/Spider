882d1cda75301069272f1586584fe791ac24dda1
Rhithick02 <rhithick02@gmail.com>
2021-07-31 13:04:03 +0530

    Coin change problems variations

---
+++ DSA/Algorithms/Dynamic_Programming/coin_change_2.cpp
@@ -0,0 +1,36 @@
+/*Given a value V, if we want to make change for V cents, and we have 
+infinite supply of each of C = { C1, C2, .. , Cm} valued coins, what 
+is the minimum number of coins to make the change? If it’s not possible 
+to make change, print -1. */
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define lli long long
+#define For(i,a,n) for(int i=(a);i<n;i++)
+#define asc(x) x.begin(),x.end()
+#define des(x) x.rbegin(),x.rend()
+#define pb push_back
+#define fi first
+#define se second
+#define mod 1000000007
+#define MAX 100001
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+int main() {
+    int n, sum, x = mod;
+    cin >> n >> sum;
+    vector <int> ar(n), dp(sum + 1, mod);
+    for(int i = 0; i < n; i++) {
+        cin >> ar[i];
+    }
+    dp[0] = 0;
+    for(int i = 1; i <= sum; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i < ar[j]) continue;
+            dp[i] = min(dp[i], 1 + dp[i - ar[j]]);
+        }
+    }
+    cout << (dp[sum] != mod ? dp[sum] : -1) << endl;
+}
